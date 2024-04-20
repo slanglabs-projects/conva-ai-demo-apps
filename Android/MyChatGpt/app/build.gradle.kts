eb7986cdb8385864a5aac7dc9703048c1bb4e0d0
vivek-slang <vivek@slanglabs.in>
2024-04-20 11:47:14 +0530

    [Android][Demo] Updated the Copilot version of Sample Android PG app
     - Mentioned Above

--- Android/MyChatGpt/app/build.gradle.kts
+++ Android/MyChatGpt/app/build.gradle.kts
@@ -70,7 +70,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.compose.foundation:foundation-android:1.6.6")
-    implementation ("in.slanglabs.conva:conva-ai-core:0.1.2")
+    implementation ("in.slanglabs.conva:conva-ai-core:0.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
